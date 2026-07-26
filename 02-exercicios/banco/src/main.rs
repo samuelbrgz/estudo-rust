08750a5d1745e9960d28cb7a9d0807534b761d32
Samuel <samuelvborgess@gmail.com>
2026-07-26 12:07:09 -0400

    Adiciona simulador de caixa eletrônico com struct e função de saque

--- 02-exercicios/banco/src/main.rs
+++ 02-exercicios/banco/src/main.rs
@@ -1,3 +1,37 @@
+struct CONTABANCARIA {
+    titular: String,
+    saldo: i32,
+    limite: i32,
+}
+
+fn sacar(conta1: &mut CONTABANCARIA, valor: i32) {
+    let total: i32 = conta1.saldo + conta1.limite;
+
+    if total >= valor {
+        if valor <= conta1.saldo {
+            conta1.saldo = conta1.saldo - valor;
+            println!("Titular: {}", conta1.titular);
+            println!("Saldo atualizado: {}", conta1.saldo);
+        } else if valor > conta1.saldo {
+            conta1.limite = conta1.limite + (conta1.saldo - valor);
+            conta1.saldo = 0;
+            println!("Titular: {}", conta1.titular);
+            println!("Saldo atualizado: {}", conta1.saldo);
+            println!("Limite de cheque especial atualizado: {}", conta1.limite);
+        }
+    } else {
+        println!("Titular: {}", conta1.titular);
+        println!("Saldo insuficiente!");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut conta1: CONTABANCARIA = CONTABANCARIA {
+        titular: String::from("Samuel"),
+        saldo: 1000,
+        limite: 500,
+    };
+    sacar(&mut conta1, 200);
+    sacar(&mut conta1, 300);
+    sacar(&mut conta1, 2000);
 }
